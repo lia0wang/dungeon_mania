ca61296d5cce0f9d86571fa4511c3b422eb2c849
Yuhan <z5271731@ad.unsw.edu.au>
2022-07-11 17:45:12 +1000

    added new Player class

---
+++ src/main/java/dungeonmania/Player.java
@@ -0,0 +1,15 @@
+package dungeonmania;
+
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class Player extends MovingEntity implements MovementBehaviour{
+   
+    public Player(Position position, int x, int y) {
+        super(position, x, y);
+    }
+
+    public void move(Direction direction) {
+        // TODO
+    }
+}
