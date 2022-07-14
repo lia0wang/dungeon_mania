041fa03df8262fdeb26d517179ba49e6dd576c41
WANG LIAO <z5306312@ad.unsw.edu.au>
2022-07-15 04:08:41 +0800

    add Inventory class and link it with Player

---
+++ src/main/java/dungeonmania/entities/moving/player/Player.java
@@ -0,0 +1,26 @@
+package dungeonmania.entities.moving.player;
+
+import dungeonmania.entities.moving.MovementBehaviour;
+import dungeonmania.entities.moving.MovingEntity;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class Player extends MovingEntity implements MovementBehaviour{
+    private Inventory inventory = new Inventory();
+   
+    public Player(Position position, int x, int y) {
+        super(position, x, y);
+    }
+
+    public void move(Direction direction) {
+        // TODO
+    }
+    
+    /**
+     * Get the inventory of the player.
+     * @return inventory
+     */
+    public Inventory getInventory() {
+        return inventory;
+    }
+}
