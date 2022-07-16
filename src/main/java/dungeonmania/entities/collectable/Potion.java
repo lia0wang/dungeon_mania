98c5ca0b9efc75abd3270c3cdbd0a37598309d66
WANG LIAO <z5306312@ad.unsw.edu.au>
2022-07-16 21:21:41 +0800

    finish constructors of Potions

--- src/main/java/dungeonmania/entities/collectable/Potion.java
+++ src/main/java/dungeonmania/entities/collectable/Potion.java
@@ -1,16 +1,18 @@
 package dungeonmania.entities.collectable;
 
+import dungeonmania.entities.moving.Player;
+
 /**
  * abstract class for Potion.
  * 
  * @author Wang Liao, <add_your_name>
  * @version 1.0
  *
- * @attributes - keyId (int) - the identifier of the key
- *
  */
 public abstract class Potion extends CollectableEntity {
     public Potion(int x, int y, String type) {
         super(x, y, type);
     }
+    
+    abstract public void use(Player player);
 }
