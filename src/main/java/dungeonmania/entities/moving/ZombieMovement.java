5ed3da20395bfde1ac2dc9b6d0dc87e6fa6fd072
Jared Mateo <jared@Jareds-MacBook-Pro.local>
2022-07-17 14:17:00 +1000

    added movement behaviours for spider player and zombie (WIP)

---
+++ src/main/java/dungeonmania/entities/moving/ZombieMovement.java
@@ -0,0 +1,13 @@
+package dungeonmania.entities.moving;
+
+import dungeonmania.util.Direction;
+
+public class ZombieMovement implements MovementBehaviour {
+
+    @Override
+    public void move(Direction direction, MovingEntity entity) {
+        
+        
+    }
+
+}
