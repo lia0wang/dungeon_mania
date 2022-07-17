ecbb1794e3a7c098d26521cafba0299c332f91f1
John Kip <z5360279@ad.unsw.edu.au>
2022-07-17 14:04:13 +1000

    goal reading updated to work for more complex cases, basic static entity testing

---
+++ src/test/java/dungeonmania/entities/StaticTests.java
@@ -0,0 +1,30 @@
+package dungeonmania.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import dungeonmania.*;
+import dungeonmania.response.models.*;
+import dungeonmania.util.Direction;
+
+public class StaticTests {
+    @Test
+    @DisplayName("Test the static entities are created")
+    public void testBasicStaticEntities() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_staticTest_basicEntities", "c_movementTest_testMovementDown");
+
+        assertEquals(new ArrayList<>(), res.getAnimations());
+        assertEquals(new ArrayList<>(), res.getBattles());
+        assertEquals(new ArrayList<>(), res.getBuildables());
+        assertEquals("d_staticTest_basicEntities", res.getDungeonName());
+        assertEquals(7, res.getEntities().size());
+        assertEquals("(:exit AND :treasure) AND (:boulders AND :enemies)", res.getGoals());
+        assertEquals(new ArrayList<>(), res.getInventory());
+    }
+}
