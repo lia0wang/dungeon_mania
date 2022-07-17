73823f1826a79e0eec9f24ea36aded05967bfb2d
WANG LIAO <z5306312@ad.unsw.edu.au>
2022-07-17 16:17:20 +0800

    update

--- src/main/java/dungeonmania/entities/moving/MovingEntity.java
+++ src/main/java/dungeonmania/entities/moving/MovingEntity.java
@@ -1,15 +1,37 @@
 package dungeonmania.entities.moving;
+
 import dungeonmania.entities.Dungeon;
 import dungeonmania.entities.Entity;
 
-public class MovingEntity extends Entity{
+public abstract class MovingEntity extends Entity{
     protected Dungeon dungeon;
-    
-    public MovingEntity(int x, int y, String type, Dungeon dungeon) {
+    private float health;
+    private float attackDamage;
+
+    public MovingEntity(int x, int y, String type, Dungeon dungeon, float health, float attackDamage) {
         super(x, y, type);
         this.dungeon = dungeon;
+        this.health = health;
+        this.attackDamage = attackDamage;
     }
+
     public Dungeon getDungeon() {
         return dungeon;
+    }
+    
+    public float getHealth() {
+        return health;
+    }
+    
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+    
+    public void setHealth(float health) {
+        this.health = health;
+    }
+    
+    public void setAttackDamage(float attackDamage) {
+        this.attackDamage = attackDamage;
     }
 }
