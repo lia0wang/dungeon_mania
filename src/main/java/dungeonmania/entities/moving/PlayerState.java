4547fac5c6d7e69c531e14d8cbc4c80616b75e38
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-07-23 09:24:31 +1000

    Automarking Results v2

--- src/main/java/dungeonmania/entities/moving/PlayerState.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.entities.moving;
-
-public interface PlayerState {
-    public void becomeInvincible(Player player);
-    public void becomeInvisible(Player player);
-    public void gameOver(Player player);
-    public void backToDefault(Player player);
-}
