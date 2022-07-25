8f3ed94938242a274a47a9c5c7cb2557318fb5df
Yuhan <z5271731@ad.unsw.edu.au>
2022-07-25 23:24:50 +1000

    update the tick()

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -97,7 +97,7 @@ public class DungeonManiaController {
             buildables.add("shield");
         }
 
-        dungeon.updateGoal();
+        
         return new DungeonResponse(dungeon.getId(), dungeon.getName(), entities, inventory, battles, buildables, dungeon.getGoals());
     }
 
@@ -115,6 +115,8 @@ public class DungeonManiaController {
     public DungeonResponse tick(Direction movementDirection) {
         dungeon.getPlayer().move(movementDirection);
         dungeon.getAllMovingEntitiesButPlayer().forEach(e -> e.move(movementDirection));
+        dungeon.pickUpItem();
+        dungeon.updateGoal();
         return getDungeonResponseModel();
     }
 
