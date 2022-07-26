7fda20a5ae051d9fc2aa470128f92f5a5f86b323
Jared Mateo <jared@Jareds-MacBook-Pro.local>
2022-07-26 15:56:02 +1000

    added tests for zombie movement

--- src/test/java/dungeonmania/entities/moving/ZombieTests.java
+++ src/test/java/dungeonmania/entities/moving/ZombieTests.java
@@ -3,6 +3,7 @@ package dungeonmania.entities.moving;
 import static dungeonmania.TestUtils.getEntities;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -22,6 +23,7 @@ public class ZombieTests {
     @Test
     @DisplayName("Test basic zombie movement")
     public void basicZombieMovement() {
+        // Index order with seed 1 is 3 -> 0 -> 1 -> 2
         Integer[] indexOrder = {0, 1, 2, 3};
         List<Integer> indexList = Arrays.asList(indexOrder);
         Collections.shuffle(indexList, new Random(1));
@@ -29,24 +31,22 @@ public class ZombieTests {
         DungeonManiaController dmc;
         dmc = new DungeonManiaController();
         DungeonResponse res = dmc.newGame("d_zombieTest_basicMovement", "c_spiderTest_basicMovement");
-        Position pos = getEntities(res, "spider").get(0).getPosition();
+        Position pos = getEntities(res, "zombie_toast").get(0).getPosition();
 
         int x = pos.getX();
         int y = pos.getY();
 
-        res = dmc.tick(Direction.UP);
-
-        assertEquals(new Position(x, y - 1), getEntities(res, "spider").get(0).getPosition());
-    }
-
-
-    public static void main(String[] args) {
-        Integer[] indexOrder = {0, 1, 2, 3};
-        List<Integer> indexList = Arrays.asList(indexOrder);
-        Collections.shuffle(indexList, new Random(1));
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(indexList.get(i));
+        // Create list of the correct position order
+        List<Position> movementTrajectory = new ArrayList<Position>();
+        movementTrajectory.add(new Position(x + 1, y));
+        movementTrajectory.add(new Position(x + 1, y - 1));
+        movementTrajectory.add(new Position(x + 1, y));
+        movementTrajectory.add(new Position(x, y));
+        
+    
+        for (int i = 0; i < movementTrajectory.size(); i++) {
+            res = dmc.tick(Direction.UP);
+            assertEquals(movementTrajectory.get(i), getEntities(res, "zombie_toast").get(0).getPosition());
         }
     }
 }
