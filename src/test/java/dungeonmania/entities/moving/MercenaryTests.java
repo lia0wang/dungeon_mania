b4d6bebc8733a5170e940c4a5e4314cdd10be843
Jared Mateo <z5345778@ad.unsw.edu.au>
2022-07-31 13:54:28 +1000

    added more tests

--- src/test/java/dungeonmania/entities/moving/MercenaryTests.java
+++ src/test/java/dungeonmania/entities/moving/MercenaryTests.java
@@ -2,6 +2,9 @@ package dungeonmania.entities.moving;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static dungeonmania.TestUtils.getEntities;
 
 import org.junit.jupiter.api.DisplayName;
@@ -30,28 +33,48 @@ public class MercenaryTests {
         DungeonManiaController dmc;
         dmc = new DungeonManiaController();
         DungeonResponse res = dmc.newGame("d_mercenaryTest_basicMovement", "c_spiderTest_basicMovement");
-        Position mercPos = getEntities(res, "mercenary").get(0).getPosition();
-        Position playerPos = getEntities(res, "player").get(0).getPosition();
-
-        // Assert that the player and mercenaries are in the right spot
-        assertEquals(new Position(5, 5), mercPos);
-        assertEquals(new Position(2, 2), playerPos);
 
         // Player moves right 1 -> merc moves UP one
         res = dmc.tick(Direction.RIGHT);
 
         // Assert that the player and mercenaries are in the right position
         assertEquals(new Position(3, 2), getEntities(res, "player").get(0).getPosition());
-        // assertEquals(new Position(5, 4), getEntities(res, "mercenary").get(0).getPosition());
+        assertEquals(new Position(5, 4), getEntities(res, "mercenary").get(0).getPosition());
 
         // Player moves right 1 -> merc moves UP one 
         res = dmc.tick(Direction.RIGHT);
         assertEquals(new Position(4, 2), getEntities(res, "player").get(0).getPosition());
-        // assertEquals(new Position(5, 3), getEntities(res, "mercenary").get(0).getPosition());
+        assertEquals(new Position(5, 3), getEntities(res, "mercenary").get(0).getPosition());
 
         // Player moves right 1 -> merc moves UP one
         res = dmc.tick(Direction.RIGHT);
         assertEquals(new Position(5, 2), getEntities(res, "player").get(0).getPosition());
-        // assertEquals(new Position(5, 2), getEntities(res, "mercenary").get(0).getPosition());
+        assertEquals(new Position(5, 2), getEntities(res, "mercenary").get(0).getPosition());
+    }
+
+    @Test
+    @DisplayName("Test shortest path w/ walls in front of it")
+    public void testDijkstraWithWalls() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+
+        // Three walls in front of the mercenary
+        DungeonResponse res = dmc.newGame("d_mercenaryTest_movementWalls", "c_spiderTest_basicMovement");
+        Position playerPos = getEntities(res, "player").get(0).getPosition();
+        int playerX = playerPos.getX() + 1;
+        int playerY = playerPos.getY();
+
+        List<Position> mercenaryTrajectory = new ArrayList<Position>();
+        mercenaryTrajectory.add(new Position(4,5));
+        mercenaryTrajectory.add(new Position(3,5));
+        mercenaryTrajectory.add(new Position(3,4));
+        mercenaryTrajectory.add(new Position(3,3));
+        mercenaryTrajectory.add(new Position(4,3));
+
+        for (int i = 0; i < mercenaryTrajectory.size(); i++) {
+            res = dmc.tick(Direction.RIGHT);
+            assertEquals(mercenaryTrajectory.get(i), getEntities(res, "mercenary").get(0).getPosition());
+            assertEquals(new Position(playerX + i, playerY), getEntities(res, "player").get(0).getPosition());
+        }
     }
 }
