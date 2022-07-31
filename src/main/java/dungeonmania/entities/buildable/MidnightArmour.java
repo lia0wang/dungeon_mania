7bc63529911a2f4eaac66547d61886d11717c0b4
Yuhan <z5271731@ad.unsw.edu.au>
2022-07-31 16:45:11 +1000

    add basic constructor and getters for buildable entities

--- src/main/java/dungeonmania/entities/buildable/MidnightArmour.java
+++ src/main/java/dungeonmania/entities/buildable/MidnightArmour.java
@@ -1,5 +1,24 @@
 package dungeonmania.entities.buildable;
 
-public class MidnightArmour {
-    
+import dungeonmania.entities.collectable.CollectableEntity;
+
+public class MidnightArmour extends CollectableEntity{
+    private int extraAttackDamage;
+    private int defense;
+
+    public MidnightArmour(int x, int y, String type, int attack, int defense) {
+        super(x, y, type);
+        this.extraAttackDamage = attack;
+        this.defense = defense;
+    }
+
+    public int getDefenseValue() {
+        return this.defense;
+    }
+
+    public int getAttackDamage() {
+        return this.extraAttackDamage;
+    }
+
+
 }
