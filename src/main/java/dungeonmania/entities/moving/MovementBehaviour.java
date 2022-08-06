0147abc7e0e99d13dcc81f725e89976fb76bd203
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-08-06 22:26:15 +1000

    Milestone 3 automarking Results

--- src/main/java/dungeonmania/entities/moving/MovementBehaviour.java
+++
@@ -1,6 +0,0 @@
-package dungeonmania.entities.moving;
-import dungeonmania.util.Direction;
-
-public interface MovementBehaviour {
-    public void move(Direction direction, MovingEntity entity);
-}
